c7925d768a68c7f68ad403fde468bdd4911a506b
Rodchenko Nikita <bira10a@gmail.com>
2022-10-07 21:12:15 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,106 @@
+$(function(){
+  $('.top-slider__list').slick({
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variablewidth: false,
+    dots: true
+  });
+})
+
+
+// BURGER
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  //Mobile Menu
+  const burger = document.querySelector('.burger'); //наша кнопка
+  const mobileMenu = document.querySelector('.menu'); //мобильное меню
+  const bodyLock = document.querySelector('body'); //ищем как селектор ТЕГА
+
+  burger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('menu--active'); //когда меню открыто
+    if (mobileMenu.classList.contains('menu--active')) { //Проверяем, есть ли у меню активный класс
+      burger.classList.add('burger--active'); //Когда открыто, иконка становится крестиком
+      bodyLock.classList.add('lock'); //Блокируем скролл при открытом меню
+    }
+    else { //Когда нету активного класса у меню
+      burger.classList.remove('burger--active'); //Возвращает в исходное состояние
+      bodyLock.classList.remove('lock'); //Разрешаем скроллить
+    }
+  });
+  //Клик вне таргета
+  document.addEventListener('click', function (e) {
+    if (e.target !== burger && e.target !== mobileMenu) {
+      burger.classList.remove('burger--active');
+      mobileMenu.classList.remove('menu--active');
+      bodyLock.classList.remove('lock');
+    }
+  });
+});
+
+
+$('.header__select').each(function () {
+  const _this = $(this),
+    selectOption = _this.find('option'),
+    selectOptionLength = selectOption.length,
+    selectedOption = selectOption.filter(':selected'),
+    duration = 350; // длительность анимации
+
+  _this.hide();
+  _this.wrap('<div class="header__select"></div>');
+  $('<div>', {
+    class: 'new-select',
+    text: _this.children('option:disabled').text()
+  }).insertAfter(_this);
+
+  const selectHead = _this.next('.new-select');
+  $('<div>', {
+    class: 'new-select__list'
+  }).insertAfter(selectHead);
+
+  const selectList = selectHead.next('.new-select__list');
+  for (let i = 1; i < selectOptionLength; i++) {
+    $('<div>', {
+      class: 'new-select__item',
+      html: $('<span>', {
+        text: selectOption.eq(i).text()
+      })
+    })
+      .attr('data-value', selectOption.eq(i).val())
+      .appendTo(selectList);
+  }
+
+  const selectItem = selectList.find('.new-select__item');
+  selectList.slideUp(0);
+  selectHead.on('click', function () {
+    if (!$(this).hasClass('on')) {
+      $(this).addClass('on');
+      selectList.slideDown(duration);
+
+      selectItem.on('click', function () {
+        let chooseItem = $(this).data('value');
+
+        $('header__select').val(chooseItem).attr('selected', 'selected');
+        selectHead.text($(this).find('span').text());
+
+        selectList.slideUp(duration);
+        selectHead.removeClass('on');
+      });
+
+    } else {
+      $(this).removeClass('on');
+      selectList.slideUp(duration);
+    }
+  });
+}); 
+
+$(window).scroll(function () {
+  if ($(document).scrollTop() > 200) {
+    $(".header__top").addClass("header__top--sticky ");
+  } else {
+    $(".header__top").removeClass("header__top--sticky ");
+  }
+});
